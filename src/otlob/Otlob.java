f64e0f465de031c6c7e4a8a96e90f3544898168a
ahmedserag2 <30501195+ahmedserag2@users.noreply.github.com>
2019-10-18 16:35:08 +0200

    Add files via upload

--- src/otlob/Otlob.java
+++ src/otlob/Otlob.java
@@ -5,6 +5,8 @@
  */
 package otlob;
 
+import java.io.IOException;
+
 /**
  *
  * @author PC
@@ -14,24 +16,17 @@ public class Otlob {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException 
     {
-           dateOfBirth b = new dateOfBirth(26,10,2000);
-       // User person = new User("ahmed","adel",b);
-       // System.out.println(person);
-       dateOfBirth b2 = new dateOfBirth(30,2,2019);
-       dateOfBirth b3 = new dateOfBirth(20,5,30);
-       
-       User users[] = new User[3];
-       users[0] = new customer("ahmed","adel",b);
-       users[1] = new admin("hossam","mostafa",b2);
-       users[2] = new customer("haada","hussein",b3);
-       
-       //for x in users
-       for(User x: users)
-       {
-           System.out.print(x);  
-       }
+      
+        restaurant r = new restaurant("mac",4);
+        
+        r.writeToRestaurant();
+                r.Readrestaurant();
+
+        meal m = new meal("burger","onions blah blah",(float)55.5,5);
+        m.writeToMeals();
+        
     }
     
 }
---
+++ src/otlob/credit.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package otlob;
+
+/**
+ *
+ * @author ahmed
+ */
+public class credit 
+{
+    private String bankName;
+    private int pinCode;
+    private String cardType;
+    
+    
+    //contructor setter
+    public credit(String BN,int PC,String CT)
+    {
+        bankName = BN;
+        PC = pinCode;
+        cardType = CT;
+    }
+    
+    
+    
+    public double amount()
+    {
+        return 0;
+    }
+    public void Transaction()
+    {
+        
+    }
+    
+}
