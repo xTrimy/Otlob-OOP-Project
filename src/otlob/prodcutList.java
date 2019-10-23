ce3af5697bef6e31110a3af580cbd21405c33526
ahmedserag2 <30501195+ahmedserag2@users.noreply.github.com>
2019-10-23 20:29:23 +0200

    added a product list which gives options to add delete and get the total price

--- src/otlob/prodcutList.java
+++ src/otlob/prodcutList.java
@@ -5,6 +5,10 @@
  */
 package otlob;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 /**
  *
@@ -15,7 +19,77 @@ public class prodcutList
     int requestedQuantity ;
     float totalPrice;
     
-    meal [] mealList = new meal[5];
+    meal  mealList[] = new meal[20];
+    
+    prodcutList()
+    {
+        
+    }
+    prodcutList(String [] id)throws IOException
+    {
+        BufferedReader r = new BufferedReader(new FileReader("meal.txt"));
+        
+        String [] list ;
+        String S;
+        //id iterator
+        int i=0;
+        //mealList iterator
+        int j=0;
+        totalPrice =0;
+        while((S = r.readLine()) != null)
+        {
+            list = S.split(",");
+
+                try
+                    {
+
+                if(list[1].equals(id[i]))
+                {
+                   
+                    mealList[i] = new meal(list[2],list[3],Float.parseFloat(list[4]),
+                    Integer.parseInt(list[5]));
+                          totalPrice += Float.parseFloat(list[4]);   
+                            i++;
+                   
+
+                }     
+
+                    }catch(Exception e){System.out.println("out of bounds");}           
+
+        }
+        requestedQuantity = i;
+        
+    }
+    
+    
+    
+    public void display()
+    {
+        try
+        {
+        for(meal m : mealList)
+        {
+            
+            m.displayMeals();
+            
+        }
+        }catch(java.lang.NullPointerException exception){}
+        System.out.println("requested quantity: "+ requestedQuantity);
+        System.out.println("total price: "+ totalPrice);
+    }
+    
+    
+    
+    public void addMeal()
+    {
+        
+        
+    }
+    
+    public void removeMeal()
+    {
+        
+    }
     
     
 }
