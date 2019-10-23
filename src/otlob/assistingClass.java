227c15bab51b371817f514d4324c6fe18a8e3061
ahmedserag2 <30501195+ahmedserag2@users.noreply.github.com>
2019-10-23 02:36:19 +0200

    added a while loop the user can now browse through the list of restaurants and meals :)

--- src/otlob/assistingClass.java
+++ src/otlob/assistingClass.java
@@ -181,9 +181,10 @@ String searchId(String Id,String fileName) throws IOException
         }
     }
      
-     
+     //ReadFile returns a 2d array 
     public String[][] ReadFile(String fileName) throws IOException
     {
+        //create the file objects
         File restaurants = new File(fileName);
         String fileData = "";
         BufferedReader reader = null;
@@ -191,24 +192,33 @@ String searchId(String Id,String fileName) throws IOException
         {
             reader = new BufferedReader(new FileReader(restaurants));
             String line = reader.readLine();
+            //read the whole file and store it into a string
             while (line != null) 
             {
+                //line separator in windows is \n
+
                 fileData = fileData + line + System.lineSeparator();
-                 
                 line = reader.readLine();
+
             }
         }catch (Exception e)
         {
             e.printStackTrace();
         }
+        //make a 1d list
         String [] list = null;
+        //make a 2d list
         String [][] result = null;
         try
         {
+            //list has all lines in a single String
             list = fileData.split(System.lineSeparator());
+            
             int size = list.length;
+            //2d array to store the rows and columns
             result = new String [size][]; 
             int count = 0;
+            //iterate and put everything in a 2d array
             for (String line : list)
             {
                 result [count] = line.split (",");
