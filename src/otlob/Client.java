47c0e1d56ac765a96eb9b5ebd4968377dc030ab5
xTrimy <Mohamed.ashraf881999@gmail.com>
2019-12-12 20:02:27 +0200

    Add simple customer support chatting system

---
+++ src/otlob/Client.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package otlob;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.*;
+import javax.swing.*;
+/**
+ *
+ * @author xTrimy
+ */
+public class Client extends JFrame{
+    static private int cid = 0;
+    private int clientId;
+    private JLabel recivedMsg;
+    private JTextField tx_Msg=new JTextField("Write message to server here");
+    InputStream is;
+    OutputStream os;
+    Socket toFromserver = null;
+    String clientName;
+    public Client (String Name) throws IOException
+    {
+        this.clientName=Name;
+        setSize(300,600);
+        setTitle("Client V 1.0" + Name);
+        recivedMsg=new JLabel("<html><div style='color:red'>Recived Message</div><br>");
+        setLayout(null);
+        recivedMsg.setBounds(20,20, 200,250);
+        add(recivedMsg);
+        JButton btnReciveMsg=new JButton("Send");
+        btnReciveMsg.setBounds(20,350,100,50);
+        tx_Msg.setBounds(btnReciveMsg.getBounds().x,btnReciveMsg.getBounds().y+50,100,50);
+        add(tx_Msg);
+        btnReciveMsg.addActionListener(new act());
+        add(btnReciveMsg);
+        
+                try {
+                    toFromserver = new Socket("127.0.0.1", 6000);
+                    is=toFromserver.getInputStream();
+                    os=toFromserver.getOutputStream();
+                    updategui t=new updategui();
+                    t.start();
+
+
+                } catch (IOException ex) {
+                    
+                }
+        PrintWriter px=new PrintWriter(os,true);
+        px.println("x,1,"+ ++cid+","+Name);
+        clientId = cid;
+    }
+     private class act implements  ActionListener
+    {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            PrintWriter pw=new PrintWriter(os,true);
+            pw.println("x1,"+clientId+","+tx_Msg.getText());
+            recivedMsg.setText(recivedMsg.getText()+"<br><span style='color:blue;'>You: </span>"+tx_Msg.getText());
+        }
+    }
+    private class updategui extends Thread
+    {
+        @Override
+        public void run()
+        {
+            while (true)
+            {
+                BufferedReader bf=new BufferedReader(new InputStreamReader(is));
+                String Message = null;
+                try {
+                    Message = bf.readLine();
+                    if (Message!=null)
+                    {
+                    recivedMsg.setText(recivedMsg.getText()+"<br>"+Message);
+                    System.out.println("Server says " +Message);
+                    
+                    }
+                } catch (IOException ex) {
+                    System.out.println(ex);
+                }
+                
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    
+                }         
+            }
+        }
+        }
+    }
+    
+
---
+++ src/otlob/CostumerSupportMain.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package otlob;
+
+import java.io.IOException;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author xTrimy
+ */
+public class CostumerSupportMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+                
+        SupportClient sc=new SupportClient("Admin1");
+        sc.setVisible(true);
+        sc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        SupportClient sc1 =new SupportClient("Admin2");
+        sc1.setVisible(true);
+        sc1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        try{
+         Thread.sleep(1000);
+        }catch(Exception e){
+        }
+        Client cl=new Client("Ahmed");
+        cl.setVisible(true);
+        cl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        Client cl2=new Client("Haidy");
+        cl2.setVisible(true);
+        cl2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+    }
+    
+}
